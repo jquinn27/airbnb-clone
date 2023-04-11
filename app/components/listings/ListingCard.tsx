6c9b89e1b85c2688d66d2b982b83c2780c4a5276
jquinn27 <jq01@outlook.com>
2023-04-10 21:39:21 -0400

    4/10

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
 import Image from "next/image";
+import HeartButton from "../HeartButtont";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -47,8 +49,10 @@ const ListingCard: React.FC<ListingCardProps> = ({
     return data.price;
   }, [reservation, data.price]);
 
-  const reservationData = useMemo(() => {
-    if (!reservation) return null;
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
 
     const start = new Date(reservation.startDate);
     const end = new Date(reservation.endDate);
@@ -69,8 +73,28 @@ const ListingCard: React.FC<ListingCardProps> = ({
             src={data.imageSrc}
             className="object-cover h-full w-full group-hover:scale-110 transition"
           />
-          <div className=""></div>
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region} , {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">
+          {reservationDate || data.category}
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
       </div>
     </div>
   );
